c3664fbc066d6c2c32f3f0e364e9965dc84ca300
JackWatts <jackwatts2000@gmail.com>
2024-10-17 15:18:45 +0100

    route fix

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -4,7 +4,6 @@ import { Results, ResultsSkeleton } from "./_components/results";
 
 
 export default function Page() {
-  console.error()
   return (
     <div className="h-full p-8 max-w-screen-2xl mx-auto">
       <Suspense fallback={<ResultsSkeleton />}>
